a365f1298e455cb641c7e30ce31a0c72a78e1469
PrashanthGanta <69982644+PrashanthGanta@users.noreply.github.com>
2022-11-23 15:42:06 +0530

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,8 +1,8 @@
 import sys
 print("Hello World")
-print("\nName of Python script:", sys.argv[0])
+print("\nName of Python script:", sys.argv[1])
 
-input_a=str(sys.argv[0])
+input_a=str(sys.argv[1])
 print("outside function call---> "+input_a)
 def sample_fun(x):
   print("inside function: "+str(x))
